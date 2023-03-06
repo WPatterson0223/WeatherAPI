fa36b81980cf56ca78939a0b7ec29269cb857cb6
Will Patterson <wlp223@aol.com>
2023-03-06 14:29:58 -0500

    exercise 2

---
+++ WeatherMapAPI/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+using static System.Net.WebRequestMethods;
+
+namespace WeatherMapAPI;
+class Program
+{
+    static void Main(string[] args)
+    {
+        var client = new HttpClient();
+
+        var weatherMapULR = "https://api.openweathermap.org/data/2.5/weather?q=Lexington&appid=d87febb8b61e578895f096b3f7ef5a7e&units=imperial";
+
+        var weatherMapResponse = client.GetStringAsync(weatherMapULR).Result;
+
+        var weatherMap = JObject.Parse(weatherMapResponse);
+
+        Console.WriteLine(weatherMap["main"]);
+    }
+}
+
